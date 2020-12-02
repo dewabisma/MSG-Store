9761ebaa819dd76071c531246b0c0c2899fa37cd
Dewa Bisma <idewagedebisma@gmail.com>
2020-12-02 21:01:06 +0800

    add productsReducer, add productAction, add productConstants, remove useState, axios module in homeScreen, add dispatch and selector of redux to use global state from store in app.js provider

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productAction.js';
 
 const HomeScreen = () => {
-  const [Products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
-  const fetchProduct = async () => {
-    const { data } = await axios.get('/api/products');
-    setProducts(data);
-  };
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    fetchProduct();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Product</h1>
-      <Row>
-        {Products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 };
