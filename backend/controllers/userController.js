8c11128977617c17fbae12a393f967920299ed37
Dewa Bisma <idewagedebisma@gmail.com>
2020-12-06 21:00:15 +0800

    add jsonwebtoken, add authorization and authentication, add authMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,42 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userSchema.js';
+import generateToken from '../utils/generateToken.js';
 
-// @desc    Fetch all products
-// @route   GET /api/products
+// @desc    Auth and get Token
+// @route   POST /api/users/login
 // @access  Public
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.send(req.body);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
   } else {
-    res.status(401).send('Invalid password or username');
+    res.status(401);
+    throw new Error('Invalid password or username');
+  }
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
   }
 });
