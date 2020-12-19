740f69f92711534720e991d030b5300707d2be27
Dewa Bisma <idewagedebisma@gmail.com>
2020-12-19 09:50:10 +0800

    add upload routes

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+import multer from 'multer';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${new Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const checkFileTypes = (file, cb) => {
+  const acceptableFileTypes = /jpg|jpeg|png/;
+  const isExtnameAccepted = acceptableFileTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const isMimetypeAccepted = acceptableFileTypes.test(file.mimetype);
+
+  if (isExtnameAccepted && isMimetypeAccepted) {
+    cb(null, true);
+  } else {
+    cb('Images only');
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileTypes(file, cb);
+  },
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`${req.file.path}`);
+});
+
+export default router;
